b19c0293e81f37d47346920ffeaf851d10c39a45
valkaryne <de20us12ex>
2019-06-15 23:24:30 +0300

    [FIN2019-16] Enhanced data converters with gson

--- app/src/main/java/com/epam/valkaryne/spectrumevo/repository/datamodel/converters/GenresConverter.kt
+++ app/src/main/java/com/epam/valkaryne/spectrumevo/repository/datamodel/converters/GenresConverter.kt
@@ -2,19 +2,19 @@ package com.epam.valkaryne.spectrumevo.repository.datamodel.converters
 
 import androidx.room.TypeConverter
 import com.epam.valkaryne.spectrumevo.repository.datamodel.Genre
+import com.google.gson.Gson
 
 class GenresConverter {
 
+    private val gson = Gson()
+
     @TypeConverter
     fun fromGenres(genres: List<Genre>): String {
-        return genres.joinToString(separator = ",") { genre -> genre.name }
+        return gson.toJson(genres)
     }
 
     @TypeConverter
     fun toGenres(data: String): List<Genre> {
-        val arrStr = data.split(",")
-        val genres = ArrayList<Genre>()
-        arrStr.forEach { genres.add(Genre(it)) }
-        return genres
+        return gson.fromJson(data, Array<Genre>::class.java).toList()
     }
 }
--- app/src/main/java/com/epam/valkaryne/spectrumevo/repository/datamodel/converters/InvolvedCompaniesConverter.kt
+++ app/src/main/java/com/epam/valkaryne/spectrumevo/repository/datamodel/converters/InvolvedCompaniesConverter.kt
@@ -1,30 +1,20 @@
 package com.epam.valkaryne.spectrumevo.repository.datamodel.converters
 
 import androidx.room.TypeConverter
-import com.epam.valkaryne.spectrumevo.repository.datamodel.Company
 import com.epam.valkaryne.spectrumevo.repository.datamodel.InvolvedCompany
+import com.google.gson.Gson
 
 class InvolvedCompaniesConverter {
 
+    private val gson = Gson()
+
     @TypeConverter
     fun fromInvolvedCompanies(involvedCompanies: List<InvolvedCompany>): String {
-        return involvedCompanies.joinToString(separator = ",") { involvedCompany ->
-            "${involvedCompany.company.name}.${involvedCompany.developer}" }
+        return gson.toJson(involvedCompanies)
     }
 
     @TypeConverter
     fun toInvolvedCompanies(data: String): List<InvolvedCompany> {
-        val involvedCompanies = ArrayList<InvolvedCompany>()
-        val arrStr = data.split(",")
-        arrStr.forEach {
-            val companyStr = it.split(".")
-            involvedCompanies.add(
-                InvolvedCompany(
-                    Company(companyStr[0]),
-                    companyStr[1].toBoolean()
-                )
-            )
-        }
-        return involvedCompanies
+        return gson.fromJson(data, Array<InvolvedCompany>::class.java).toList()
     }
 }
